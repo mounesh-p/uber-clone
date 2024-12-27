ae1fd527527c5b81646bde24d655af20dc52d6bf
Mounesh Pattar <mouneshp11@gmail.com>
2024-12-27 14:32:54 +0530

    dd

--- backend/app.js
+++ backend/app.js
@@ -14,19 +14,13 @@ connectToDb();
 const app  = express();
 
 // Configure CORS
-const allowedOrigins = ['https://uber-clone-three-theta.vercel.app'];
-app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+const corsOptions = {
+    origin: 'https://uber-clone-3tah.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
+  app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());
